cf464503a1d6ca09767d8d46d8493ff6ba6433ad
VM User <vmuser@vmware.com>
2014-11-17 17:57:31 +0800

    new test file

--- DSnP_hw4/tests/wtest.sh
+++ DSnP_hw4/tests/wtest.sh
@@ -1,12 +1,15 @@
 ../memTest -f do1 &> J1.log
 ../memTest -f do2 &> J2.log
+../memTest -f do3 &> J3.log
 ../memTest -f do4 &> J4.log
 ../memTest -f do5 &> J5.log
 ../ref/memTest -f do1 &> H1.log
 ../ref/memTest -f do2 &> H2.log
+../ref/memTest -f do3 &> H3.log
 ../ref/memTest -f do4 &> H4.log
 ../ref/memTest -f do5 &> H5.log
 diff J1.log H1.log 
 diff J2.log H2.log 
+diff J3.log H3.log 
 diff J4.log H4.log 
 diff J5.log H5.log
