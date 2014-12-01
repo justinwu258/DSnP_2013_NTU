f1a3b417a59caada5dcf4cf39dbc841e397d5666
justinwu258 <justinwu258@gmail.com>
2014-12-01 16:42:36 +0800

    dlist add method implement , some small bugs

--- DSnP_hw5/src/util/dlist.h
+++ DSnP_hw5/src/util/dlist.h
@@ -10,6 +10,7 @@
 #define DLIST_H
 
 #include <cassert>
+#include <unistd.h>
 
 template <class T> class DList;
 
@@ -55,28 +56,107 @@ public:
 
       // TODO: implement these overloaded operators
       const T& operator * () const { return *(this); }
-      T& operator * () { return _node->_data; }
-      iterator& operator ++ () { return *(this); }
+      T& operator * () { 
+            // cout << "T& operator * ()" << endl; 
+            return _node->_data; 
+      }
+      iterator& operator ++ () {
+         //cout << " iterator& operator ++ ()" << endl; 
+         _node = _node->_next;
+         return *(this); 
+      }
       iterator operator ++ (int) { return *(this); }
-      iterator& operator -- () { return *(this); }
-      iterator operator -- (int) { return *(this); }
+      iterator& operator -- () { 
+        //cout << " iterator& operator -- ()" << endl; 
+        _node = _node->_prev;
+        return *(this); 
+        }
+      iterator operator -- (int) { 
+        cout << " iterator operator -- (int)" << endl;
+        _node = _node->_prev; 
+         return *(this); 
+      }
 
-      iterator& operator = (const iterator& i) { return *(this); }
+      iterator& operator = (const iterator& i) { 
+            _node = i._node;
+            return *(this); 
+      }
 
-      bool operator != (const iterator& i) const { return false; }
-      bool operator == (const iterator& i) const { return false; }
+      bool operator != (const iterator& i) const {
+            //usleep(1*10^5); 
+            //cout << "bool operator != " << endl; 
+            return _node!=i._node; 
+      }
+      bool operator == (const iterator& i) const { 
+            cout << "bool operator == " << endl; 
+            return _node==i._node;
+      }
 
    private:
       DListNode<T>* _node;
    };
 
    // TODO: implement these functions
-   iterator begin() const { return 0; }
-   iterator end() const { return 0; }
-   bool empty() const { return false; }
+   iterator begin() const { 
+        return iterator(_head->_next); 
+   }
+   iterator end() const { 
+        // ***
+        //iterator(_next) = begin();
+        //iterator(_prev) = _node->_data;
+        return iterator(_head); 
+   }
+   bool empty() const { 
+        if (_head->_next == _head)
+        {   
+            cout << "empty " << endl;
+            return true;
+        }
+        else     
+            return false; 
+   }
    size_t size() const {  return 0; }
 
-   void push_back(const T& x) { }
+   void push_back(const T& x) {
+      //if(_head->_prev == _head) { 
+      if(empty()) { 
+                    cout << "a. "<<endl;
+            DListNode<T>* t = new DListNode<T>(x,_head,_head);
+            _head->_next = t;
+            _head->_prev = t;
+            //_head->_prev = _head;  // not necessary , but equal to this 
+            //_head = t;
+        }
+ //       else if(_head->_prev == _head->_next){    // end().prev == begin()
+ //                   cout << "b." <<endl;
+ //           iterator iter = _head->_prev;
+ //           if(iter._node->_next == _head) {
+ //                   cout << "5. iter._node" << iter._node <<endl;
+ //                   cout << "add new node"  << endl;
+ //                   DListNode<T>* t = new DListNode<T>(x,iter._node,_head);
+ //                   //iter._node->_next = t;  //_head->_prev->_next
+ //                   //iter._node = t;         //_head->_prev 
+ //                   _head->_prev->_next = t;
+ //                   _head->_prev= t;
+ //           }
+ //       }
+        else {
+            iterator iter = end();
+                    //cout << "0. iter._node->_data = " << iter._node->_data <<endl;
+            for(iter = end(); (iter != begin()) || (_head->_prev == _head->_next); --iter) { //(_head->_prev == _head->_next) means only 
+                    //cout << "1. iter._node->_data = " << iter._node->_data <<endl;         //dummy node & another one node
+                    //cout << "1. iter._node = " << iter._node <<endl;
+               if(iter._node->_next == _head) {
+                    //cout << "2. iter._node" << iter._node <<endl;
+                    //cout << "add new node"  << endl;
+                    DListNode<T>* t = new DListNode<T>(x,iter._node,_head);
+                    _head->_prev->_next = t;
+                    _head->_prev= t;
+               }
+            }
+            
+        }
+   }
    void pop_front() { }
    void pop_back() { }
 
