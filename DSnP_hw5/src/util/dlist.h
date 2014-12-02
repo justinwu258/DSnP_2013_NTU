0fd5e060847f80dd4a03d25b84663fd57d9b769b
justinwu258 <justinwu258@gmail.com>
2014-12-02 19:32:00 +0800

    remove nonecessary cout

--- DSnP_hw5/src/util/dlist.h
+++ DSnP_hw5/src/util/dlist.h
@@ -68,7 +68,7 @@ public:
          return *(this); 
       }
       iterator operator ++ (int) {
-        cout << " iterator operator ++ (int)" << endl; 
+        //cout << " iterator operator ++ (int)" << endl; 
         iterator iter = *(this);
         _node = _node->_next; 
         return iter;
@@ -97,7 +97,7 @@ public:
             return _node!=i._node; 
       }
       bool operator == (const iterator& i) const { 
-            cout << "bool operator == " << endl; 
+         //   cout << "bool operator == " << endl; 
             return _node==i._node;
       }
 
@@ -203,6 +203,9 @@ public:
         for(iterator i = _head ; i!= _head->_prev; ++i ) { 
            // cout << "*i.node = " << i._node << " , x = " << x << endl;
             if(i._node->_data == x) {
+                //cout << "i._node->_data" << i._node->_data << endl;
+                //cout << "i._node->_data" << *i._node->_data << endl;
+                //cout << "i._node->_data" << *i._node << endl;
                 erase(i);
                 return true;
             }   
@@ -222,23 +225,146 @@ public:
         _head->_prev = _head->_next = _head; // _head is a dummy node 
    }  // delete all nodes except for the dummy node
 
-   void sort() const { 
-
-       //   if(!empty()){
-       //       for(iterator i = _head ; i!= _head->_prev; ++i ) { 
-       //           for(iterator j = _head->next ; j!= _head->_prev; ++i ) { 
-       //               if(i._node->_data == x) {
-
-       //               }   
-       //           }   
-       //       }   
-       //   }
+   void sort() { 
+          size_t count = 0 , nowNode = 0;
+          if(!empty()){
+              iterator i = _head , tmpIter = _head , tmpPrev , tmpNext;
+              //DListNode<T>* minNode = i._node;
+              for(i = _head ; i!= _head->_prev; ++i ) { 
+                  DListNode<T>* minNode = i._node ,  *tmpNode = _head->_prev;
+                  tmpIter = i;
+            //      cout << "_head = " << _head << endl;
+            //      cout << "_head = " << _head->_data << endl;
+            //      cout << "_head->_next = " << _head->_next << endl;
+            //      cout << "_head->_prev = " << _head->_prev << endl;
+            //      cout << "i._node = " << i._node << endl;
+            //      cout << "i._node->_next = " << i._node->_next << endl;
+            //      cout << "i._node->_prev = " << i._node->_prev << endl;
+                  for(iterator j = i._node->_next ; j!= _head->_prev; ++j ) {
+                           // usleep(20000);
+                        //cout << "sort count = " << count << endl; 
+                        //if( !( minNode->_data < j._node->_data )){
+                        if( (j._node->_data) < (minNode->_data )){
+                           // usleep(30000);
+                     //       cout << "find minNode , i._node->_data = " << i._node->_data << endl;
+                     //       cout << "find minNode , j._node->_data = " << j._node->_data << endl;
+                            minNode = j._node;
+                            tmpIter = j;
+                        }
+                       //if(i._node->_data != j._node->_data) {
+                       //  compare(i,j);
+                       // }
+                       ++count;    
+                  }
+                   //    cout << "1. sort count = " << count << endl; 
+                   //    cout << "1. nowNode = " << nowNode << endl; 
+                        ++nowNode;
+                   //    cout << "final minNode = " << minNode->_data << endl << endl;
+                  //tmpNode = i._node;
+                  tmpPrev = (i._node->_prev);   
+                  tmpNext = i._node->_next;   
+                  if(minNode == tmpNext._node){
+                      tmpNext = i._node;
+                  }
+                  //tmpNode->_prev = (i._node->_prev);   
+                  //tmpNode->_next = i._node->_next;   
+                  //cout << "1 tmpNode._node = " << tmpNode << endl;
+                  //cout << "1 tmpPrev = " << tmpPrev << endl;
+                  //cout << "1 tmpNext = " << tmpNext << endl;
+                  //cout << "1 tmpNode->_next = " << tmpNode->_next << endl;
+                  //cout << "1 tmpNode->_prev = " << tmpNode->_prev << endl;
+                  // change start
+                  if(i._node->_next == minNode){
+                      i._node->_prev = minNode;
+                      i._node->_next = minNode->_next;
+                      minNode->_next = i._node;
+                      minNode->_prev = tmpPrev._node;
+                  } else {
+                      i._node->_prev = minNode->_prev;
+                      minNode->_prev->_next = i._node;
+                      i._node->_next = minNode->_next;
+                      minNode->_next->_prev = i._node;
+                  }
+                  //i._node = minNode;             
+     //             cout << "1-1 tmpNode._node = " << tmpNode << endl;
+     //             cout << "2 i._node = " << i._node << endl;
+     //             cout << "_head = " << _head << endl;
+     //             cout << "1-1 tmpNode->_next = " << tmpNode->_next << endl;
+     //             cout << "1-1 tmpNode->_prev = " << tmpNode->_prev << endl;
+     //             cout << "1-1 tmpPrev = " << tmpPrev._node << endl;
+     //             cout << "1-1 tmpNext = " << tmpNext._node << endl;
+     //             cout << "1-1 minNode._node = " << minNode << endl;
+     //             cout << "1-1 minNode->_next = " << minNode->_next << endl;
+     //             cout << "1-1 minNode->_prev = " << minNode->_prev << endl;
+     //
+                  minNode->_prev = tmpPrev._node; 
+                  tmpPrev._node->_next = minNode;
+                  minNode->_next = tmpNext._node;   
+                  tmpNext._node->_prev = minNode;
+          //        cout << "2 i._node = " << i._node << endl;
+          //        cout << "2 i._node->_next = " << i._node->_next << endl;
+          //        cout << "2 i._node->_prev = " << i._node->_prev << endl;
+          //        cout << "2 tmpNode._node = " << tmpNode << endl;
+          //        cout << "2 tmpNode->_next = " << tmpNode->_next << endl;
+          //        cout << "2 tmpNode->_prev = " << tmpNode->_prev << endl;
+                  if(i == _head)
+                  {
+                      //cout << "i == _head" << endl;
+                      _head = minNode;
+                      //_head->_prev= minNode->_prev;
+                      //_head->_next = minNode->_next;
+                  }
+                  i = minNode;
+          //        cout << "_head = " << _head << endl;
+          //        cout << "_head = " << _head->_data << endl;
+          //        cout << "_head->_next = " << _head->_next << endl;
+          //        cout << "_head->_prev = " << _head->_prev << endl;
+          //        cout << "minNode = " << minNode << endl;
+          //        cout << "_head = " << minNode->_data << endl;
+          //        cout << "_head->_next = " << minNode->_next << endl;
+          //        cout << "_head->_prev = " << minNode->_prev << endl;
+          //        cout << "3 i._node = " << i._node << endl;
+          //        cout << "3 i._node->_next = " << i._node->_next << endl;
+          //        cout << "3 i._node->_prev = " << i._node->_prev << endl;
+                  //minNode = tmpNode;
+                  //i = tmpIter;
+                  //_head = minNode; 
+                  //minNode = _head;
+                  //i = minNode;
+                  //i._node = minNode;
+                  // change done
+                  //i._node = minNode; //recover i 
+              }
+                       //cout << "2. sort count = " << count << endl; 
+          }
 
    
    }
 
 private:
    DListNode<T>*  _head;  // = dummy node if list is empty
+     template <class InputIterator1, class InputIterator2>
+     bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+                                   InputIterator2 first2, InputIterator2 last2)
+    {
+      while (first1!=last1)
+      {
+        if (first2==last2 || *first2<*first1) return false;
+        else if (*first1<*first2) return true;
+        ++first1; ++first2;
+      }
+      return (first2!=last2);
+    }
+       void compare(iterator i , iterator j ){
+    //      l=size(i);
+    //      l2=size(j);
+    //      for(k=0;k<min{size(i),size(j)},k++)
+    //      {   
+    //          compare(i[l-k],j[l2-k]);//if(same do next, else do swap)
+    //          if(l==k) //do swap
+    //          else if(l2==k)//do nothing
+    //      }   
+       } 
 
    // [OPTIONAL TODO] helper functions; called by public member functions
 };
