38cfac2439432a4ca371e571677be23347370999
justinwu258 <justinwu258@gmail.com>
2014-12-14 17:04:16 +0800

    1. shellScript update 2. dofile update

--- DSnP_hw6/Hdo.sh
+++ DSnP_hw6/Hdo.sh
@@ -1,4 +1,5 @@
 ./ref/cirTest -f doJ
 ./ref/cirTest -f doStrash06
+./ref/cirTest -f doJust
 #./ref/cirTest -f doTest
 #./ref/cirTest -f doTestNetList
--- DSnP_hw6/Jdo.sh
+++ DSnP_hw6/Jdo.sh
@@ -1,4 +1,6 @@
 ./cirTest -f doJ
 ./cirTest -f doStrash06
+./cirTest -f doJust
+#./cirTest -f doSim13
 #./cirTest -f doTest
 #./ref/cirTest -f doTestNetList
