dac3120f0b1d5e1aa901422dec0f4e1ff24db872
justinwu258 <justinwu258@gmail.com>
2014-12-16 16:16:26 +0800

    match this two script content

--- DSnP_hw6/H_cirGate.sh
+++ DSnP_hw6/H_cirGate.sh
@@ -1,5 +1,6 @@
 ./ref/cirTest -f doGate
 ./ref/cirTest -f doOK01
 ./ref/cirTest -f doFlt02
+./ref/cirTest -f doFlt05
 ./ref/cirTest -f doJustFanout
 ./ref/cirTest -f doFanout
--- DSnP_hw6/J_cirGate.sh
+++ DSnP_hw6/J_cirGate.sh
@@ -1,5 +1,6 @@
 ./cirTest -f doGate
 ./cirTest -f doOK01
+./cirTest -f doFlt02
 ./cirTest -f doFlt05
 ./cirTest -f doJustFanout
 ./cirTest -f doFanout
