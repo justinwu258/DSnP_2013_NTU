292e9a598e6bec0b267d89a7303260baa53ef661
justinwu258 <justinwu258@gmail.com>
2014-12-16 16:12:33 +0800

    remove comment out for  script

--- DSnP_hw6/H_cirGate.sh
+++ DSnP_hw6/H_cirGate.sh
@@ -1,5 +1,5 @@
-#./ref/cirTest -f doGate
-#./ref/cirTest -f doOK01
-#./ref/cirTest -f doFlt02
-#./ref/cirTest -f doJustFanout
+./ref/cirTest -f doGate
+./ref/cirTest -f doOK01
+./ref/cirTest -f doFlt02
+./ref/cirTest -f doJustFanout
 ./ref/cirTest -f doFanout
--- DSnP_hw6/diff.sh
+++ DSnP_hw6/diff.sh
@@ -1,9 +1,9 @@
-#./Jdo.sh &> J_test1.log
-#./Hdo.sh &> H_test1.log
+./Jdo.sh &> J_test1.log
+./Hdo.sh &> H_test1.log
 time ./J_cirGate.sh &> J_testGate.log
 time ./H_cirGate.sh &> H_testGate.log
-#./J_Wr.sh &> J_testWr.log
-#./H_Wr.sh &> H_testWr.log
-#diff J_test1.log H_test1.log
+./J_Wr.sh &> J_testWr.log
+./H_Wr.sh &> H_testWr.log
+diff J_test1.log H_test1.log
 diff J_testGate.log H_testGate.log
-#diff J_testWr.log H_testWr.log
+diff J_testWr.log H_testWr.log
