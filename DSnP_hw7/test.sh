078c40a0a87acf816d51711f7e557ab7847ce028
VM User <vmuser@vmware.com>
2014-12-30 09:52:20 +0800

    add script & dofile

---
+++ DSnP_hw7/test.sh
@@ -0,0 +1,13 @@
+make
+#./taskMgr -f doJ2 &> j2.txt
+#./ref/taskMgr -f doJ2 &> h2.txt
+#diff j2.txt h2.txt
+#echo "----- J2 done -----"
+#./taskMgr -f doJust &> jJust.txt
+#./ref/taskMgr -f doJust &> hJust.txt
+#diff jJust.txt hJust.txt
+#echo "----- Just done -----"
+./taskMgr -f doJ3 &> j3.txt
+./ref/taskMgr -f doJ3 &> h3.txt
+diff j3.txt h3.txt
+echo "----- J3 done -----"
