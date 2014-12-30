6a68092e426061642ae370bd69ec635a98c8b971
VM User <vmuser@vmware.com>
2014-12-30 17:49:28 +0800

    script & dofile update

---
+++ DSnP_hw7/basic_doTest.sh
@@ -0,0 +1,18 @@
+make
+#./taskMgr -f dofiles/do1 &> jDo1.txt
+#./ref/taskMgr -f dofiles/do1 &> hDo1.txt
+#diff jDo1.txt hDo1.txt
+#echo "----- Do1 done -----"
+./taskMgr -f dofiles/do2 &> jDo2.txt
+./ref/taskMgr -f dofiles/do2 &> hDo2.txt
+diff jDo2.txt hDo2.txt
+#vi jDo2.txt
+echo "----- Do2 done -----"
+#./taskMgr -f dofiles/do3 &> jDo3.txt
+#./ref/taskMgr -f dofiles/do3 &> hDo3.txt
+#diff jDo3.txt hDo3.txt
+#vi jDo3.txt
+#echo "----- Do3 done -----"
+
+#vi hBigTaski.txt
+#echo "----- JBigTaski done -----"
