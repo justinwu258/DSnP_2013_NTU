d485d5b87274e2fdc4551f69059fe47b99ccc362
justinwu258 <justinwu258@gmail.com>
2015-01-12 13:49:35 +0800

    add hash function implement

--- fraig/src/util/myHashMap.h
+++ fraig/src/util/myHashMap.h
@@ -33,6 +33,29 @@ using namespace std;
 // private:
 // };
 //
+ class HashKey
+ {
+ public:
+    HashKey() {}
+ 
+    size_t operator() () const {
+         
+        return 0; 
+    }
+ 
+    bool operator == (const HashKey& k) const {
+        bool isTypeEqual = _type && k._type;
+        bool isFaninsEqual = ((_fanin1 == k._fanin1) && (_fanin2 == k._fanin2)) || 
+                             ((_fanin2 == k._fanin1) && (_fanin1 == k._fanin2));
+        return  isTypeEqual && isFaninsEqual; 
+    }
+ 
+ private:
+    size_t _fanin1;
+    size_t _fanin2;
+    string _type;
+ };
+
 template <class HashKey, class HashData>
 class HashMap
 {
@@ -75,15 +98,68 @@ public:
    const vector<HashNode>& operator [](size_t i) const { return _buckets[i]; }
 
    // TODO: implement these functions
-   //
+   //u
    // Point to the first valid data
    iterator begin() const { iterator(); }
    // Pass the end
    iterator end() const { iterator(); }
    // return true if no valid data
-   bool empty() const { return true; }
+   bool empty() const { 
+         for(int i=0 ; i <  numBuckets() ; i++) {
+             if( !_buckets[i].empty()){
+                 return false;
+             }
+        } 
+        return true; 
+   }
    // number of valid data
-   size_t size() const { size_t s = 0; return s; }
+   size_t size() const { 
+        size_t s = 0; 
+        for(int i=0 ; i <  numBuckets() ; i++) {
+             if( !_buckets[i].empty()){
+                s += _buckets[i].size();      
+             }   
+        }   
+        return s;  
+   }
+   bool check(const HashKey& k, HashData& d) const {
+        size_t bucketIdx = bucketNum(k);
+        size_t bucketSize = _buckets[bucketIdx].size();
+            if(!_buckets[bucketIdx].empty()){
+                for(int i = 0; i < bucketSize ; i++) {
+                    if(_buckets[bucketIdx][i].first == k){
+                        d = _buckets[bucketIdx][i].second;
+                        return true;
+                    }
+                }
+            }
+        //}
+        return false;
+   } 
+   bool insert(const HashKey& k, const HashData& d) {
+        size_t bucketIdx = bucketNum(k);
+        //size_t bucketSize = _buckets[bucketIdx].size();
+         if(check(k,d)) {
+                    return false;
+         }  
+         HashNode myNode(k,d); 
+         _buckets[bucketIdx].push_back(myNode);
+         //++bucketSize;
+         return true; 
+   }   
+
+   void myPrintAll() const {
+         for(int i=0 ; i <  numBuckets() ; i++) {
+             if( !_buckets[i].empty()){
+                 for(typename vector<HashNode>::iterator it =  _buckets[i].begin(); it !=  _buckets[i].end() ; it++ ) {
+//                     cout << "Just print" << endl;
+                     cout << (*it) << endl;
+                 }
+             }
+
+         }
+    }
+
 
 private:
    // Do not add any extra data member
