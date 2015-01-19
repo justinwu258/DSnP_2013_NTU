c8cffde96c77661d24cd179dca1de58b75cb9fcf
justinwu258 <justinwu258@gmail.com>
2015-01-19 23:46:44 +0800

    add test sim13

--- fraig/diffSim.sh
+++ fraig/diffSim.sh
@@ -5,10 +5,22 @@ vi myDiffSimulSim03.log
 echo "diff doSimulSim03 done"
 ./fraig -f doSimulSim10 &> J_SimulSim10.log
 ./ref/fraig -f doSimulSim10 &> H_SimulSim10.log
-#diff J_SimulSim10.log H_SimulSim10.log &> myDiffSimulSim10.log
+diff J_SimulSim10.log H_SimulSim10.log #&> myDiffSimulSim10.log
 #vi mydiffSimulSim10.log
-vimdiff J_SimulSim10.log H_SimulSim10.log #&> myDiffSimulSim10.log
+#vimdiff J_SimulSim10.log H_SimulSim10.log #&> myDiffSimulSim10.log
 echo "diff doSimulSim10 done"
+./fraig -f doSimulSim13auto &> J_SimulSim13auto.log
+./ref/fraig -f doSimulSim13auto &> H_SimulSim13auto.log
+#diff J_SimulSim13auto.log H_SimulSim13auto.log &> myDiffSimulSim13auto.log
+#vi mydiffSimulSim13auto.log
+vimdiff J_SimulSim13auto.log H_SimulSim13auto.log #&> myDiffSimulSim13auto.log
+echo "diff doSimulSim13auto done"
+##./fraig -f doSimulSim13 &> J_SimulSim13.log
+##./ref/fraig -f doSimulSim13 &> H_SimulSim13.log
+###diff J_SimulSim13.log H_SimulSim13.log &> myDiffSimulSim13.log
+###vi mydiffSimulSim13.log
+##vimdiff J_SimulSim13.log H_SimulSim13.log #&> myDiffSimulSim13.log
+##echo "diff doSimulSim13 done"
 #./fraig -f doSimulSim09 &> J_SimulSim09.log
 #./ref/fraig -f doSimulSim09 &> H_SimulSim09.log
 #diff J_SimulSim09.log H_SimulSim09.log &> myDiffSimulSim09.log
