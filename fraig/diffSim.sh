fb6eba2d5c4276e8957f5f8c339e1fa4206c61fc
justinwu258 <justinwu258@gmail.com>
2015-01-20 02:27:33 +0800

    add simulate test

--- fraig/diffSim.sh
+++ fraig/diffSim.sh
@@ -1,7 +1,7 @@
 ./fraig -f doSimulSim03 &> J_SimulSim03.log
 ./ref/fraig -f doSimulSim03 &> H_SimulSim03.log
-diff J_SimulSim03.log H_SimulSim03.log &> myDiffSimulSim03.log
-vi myDiffSimulSim03.log
+diff J_SimulSim03.log H_SimulSim03.log #&> myDiffSimulSim03.log
+#vi myDiffSimulSim03.log
 echo "diff doSimulSim03 done"
 ./fraig -f doSimulSim10 &> J_SimulSim10.log
 ./ref/fraig -f doSimulSim10 &> H_SimulSim10.log
@@ -15,6 +15,12 @@ echo "diff doSimulSim10 done"
 #vi mydiffSimulSim13auto.log
 vimdiff J_SimulSim13auto.log H_SimulSim13auto.log #&> myDiffSimulSim13auto.log
 echo "diff doSimulSim13auto done"
+./fraig -f doSimulSim13auto_random &> J_SimulSim13auto_random.log
+./ref/fraig -f doSimulSim13auto_random &> H_SimulSim13auto_random.log
+#diff J_SimulSim13auto_random.log H_SimulSim13auto_random.log &> myDiffSimulSim13auto_random.log
+#vi mydiffSimulSim13auto_random.log
+vimdiff J_SimulSim13auto_random.log H_SimulSim13auto_random.log #&> myDiffSimulSim13auto_random.log
+echo "diff doSimulSim13auto_random done"
 ##./fraig -f doSimulSim13 &> J_SimulSim13.log
 ##./ref/fraig -f doSimulSim13 &> H_SimulSim13.log
 ###diff J_SimulSim13.log H_SimulSim13.log &> myDiffSimulSim13.log
--- fraig/test.cpp
+++ fraig/test.cpp
@@ -15,6 +15,26 @@ int main(){
     myFile << "cirp -fec" << endl;
     myFile << "cirp -n" << endl;
     myFile << "cirsim -f tests.fraig/pattern.13 -o outSim13.log" << endl;
+    myFile << "cirp -fec" << endl;
+    for(int i = 1; i < gateNum; ++i){
+        if(i%150 == 0 || i%605 == 0)
+            myFile << "cirg " << i << endl;
+    }
+    myFile << "usage" << endl;
+    myFile << "q -f" << endl;
+    myFile.close();
+    
+    myFile.open("doSimulSim13auto_random",ios::out);
+    myFile << "cirr -r tests.fraig/sim13.aag" << endl;
+    for(int i = 1; i < gateNum; ++i){
+        if(i%150 == 0 || i%605 == 0)
+            myFile << "cirg " << i << endl;
+    }
+    myFile << "cirp" << endl;
+    myFile << "cirp -fec" << endl;
+    myFile << "cirp -n" << endl;
+    myFile << "cirsim -r -o outSim13_random.log" << endl;
+    myFile << "cirp -fec" << endl;
     for(int i = 1; i < gateNum; ++i){
         if(i%150 == 0 || i%605 == 0)
             myFile << "cirg " << i << endl;
